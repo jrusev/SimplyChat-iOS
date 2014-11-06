434d01513acfdd95333b30c9e531866ae4e0e096
JR <jrusev@gmail.com>
2014-11-07 00:52:25 +0200

    deserialized JSON

--- HttpRequester/HttpRequester/HttpReceiver.m
+++ HttpRequester/HttpRequester/HttpReceiver.m
@@ -12,58 +12,27 @@
 
 #pragma mark - HttpReceiverDelegate
 
+- (NSDictionary *)getJson:(NSData *)data {
+    NSError *error = nil;
+    id object = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+    
+    if(error || ![object isKindOfClass:[NSDictionary class]]) {
+        NSLog(@"There was a problem deserializing from JSON");
+        return nil;
+    }
+
+    NSDictionary *results = object;
+    return results;
+}
+
 -(void)receivedData:(NSData *)data {
     self.data = data;
-    NSString* response = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-    NSLog(@"Response received: %@", response);
+//    NSString* response = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+//    NSLog(@"Response received: %@", response);
     
-    NSData *returnedData = data;
-    
-    // probably check here that returnedData isn't nil; attempting
-    // NSJSONSerialization with nil data raises an exception, and who
-    // knows how your third-party library intends to react?
-    
-    if(NSClassFromString(@"NSJSONSerialization"))
-    {
-        NSError *error = nil;
-        id object = [NSJSONSerialization
-                     JSONObjectWithData:returnedData
-                     options:0
-                     error:&error];
-        
-        if(error) { /* JSON was malformed, act appropriately here */ }
-        
-        // the originating poster wants to deal with dictionaries;
-        // assuming you do too then something like this is the first
-        // validation step:
-        if([object isKindOfClass:[NSDictionary class]])
-        {
-            NSDictionary *results = object;
-            /* proceed with results as you like; the assignment to
-             an explicit NSDictionary * is artificial step to get
-             compile-time checking from here on down (and better autocompletion
-             when editing). You could have just made object an NSDictionary *
-             in the first place but stylistically you might prefer to keep
-             the question of type open until it's confirmed */
-            NSLog(@"JSON object: %@", results);
-            NSLog(@"access_token: %@", results[@"access_token"]);
-        }
-        else
-        {
-            /* there's no guarantee that the outermost object in a JSON
-             packet will be a dictionary; if we get here then it wasn't,
-             so 'object' shouldn't be treated as an NSDictionary; probably
-             you need to report a suitable error condition */
-        }
-    }
-    else
-    {
-        // the user is using iOS 4; we'll need to use a third-party solution.
-        // If you don't intend to support iOS 4 then get rid of this entire
-        // conditional and just jump straight to
-        // NSError *error = nil;
-        // [NSJSONSerialization JSONObjectWithData:...
-    }
+    NSDictionary *jsonObj = [self getJson:data];
+    NSLog(@"JSON received: %@", jsonObj);
+    NSLog(@"access_token: %@", jsonObj[@"access_token"]);
 }
 
 - (void)failedWithError:(NSError *)error {
