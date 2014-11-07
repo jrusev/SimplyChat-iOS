44dea00064295bd9fc938cc225db26fae87ae543
JR <jrusev@gmail.com>
2014-11-07 03:13:45 +0200

    deserialized JSON

--- HttpRequester/HttpRequester/HttpReceiver.m
+++ HttpRequester/HttpRequester/HttpReceiver.m
@@ -8,24 +8,42 @@
 
 #import "HttpReceiver.h"
 
+#define URL_AUTH_TOKEN @"http://localhost:1337/auth/token"
+#define URL_USERS_PROFILE @"http://localhost:1337/api/users/profile"
+
+@interface HttpReceiver()
+
+@property (nonatomic, strong) HttpRequester *requester;
+
+@end
+
 @implementation HttpReceiver
 
+- (HttpRequester *)requester {
+    if (!_requester) {
+        _requester = [[HttpRequester alloc] init];
+    }
+    return _requester;
+}
+
 - (void)connect {
-    HttpRequester *requester = [[HttpRequester alloc] init];
     
-    NSString *postUrl = @"http://localhost:1337/auth/token";
     NSString *postContent = [NSString stringWithFormat:@"&grant_type=%@&client_id=%@&client_secret=%@&username=%@&password=%@",@"password",@"mobileV1",@"abc123456",@"admin", @"admin"];
-    NSString *getUrl = @"http://localhost:1337/api/users/profile";
     
-    NSString *token = @"cbMlxxmFBbZ2p3YOuSfEfOeMYFppdEpQOUrDGJXW+jo=";
-    NSDictionary *headers = @{ @"Authorization": [NSString stringWithFormat:@"Bearer %@", token] };
-    [requester httpGetWithURL:getUrl headers:headers delegate:self];
-    //[requester httpPostWithURL:postUrl content:postContent delegate:self];
+    [self.requester httpPostWithURL:URL_AUTH_TOKEN content:postContent callback:^(NSError *error, NSData *data) {
+        if (error) {
+            [self failedWithError:error];
+        } else {
+            [self receivedToken:data];
+        }
+    }];
     
     // Create a loop until we get the data back
     NSDate *futureTime = [NSDate dateWithTimeIntervalSinceNow:1.0];
     [[NSRunLoop currentRunLoop] runUntilDate:futureTime];
 }
+
+#pragma mark - private methods
 
 - (NSDictionary *)getJson:(NSData *)data {
     NSError *error = nil;
@@ -40,16 +58,36 @@
     return results;
 }
 
+-(void)receivedToken:(NSData *)data {
+    
+    NSDictionary *jsonObj = [self getJson:data];
+    NSString *token = jsonObj[@"access_token"];
+    NSLog(@"access_token: %@", token);
+   
+   
+    //NSString *token = @"cbMlxxmFBbZ2p3YOuSfEfOeMYFppdEpQOUrDGJXW+jo=";
+    NSDictionary *headers = @{ @"Authorization": [NSString stringWithFormat:@"Bearer %@", token] };
+    
+    [self.requester httpGetWithURL:URL_USERS_PROFILE headers:headers callback:^(NSError *error, NSData *data) {
+        if (error) {
+            [self failedWithError:error];
+        } else {
+            [self receivedData:data];
+        }
+    }];
+}
+
+- (void)printData:(NSData *)data {
+    NSString* dataString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"Response received: %@", dataString);
+}
+
 #pragma mark - HttpReceiverDelegate
 
 -(void)receivedData:(NSData *)data {
-
-//    NSString* response = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-//    NSLog(@"Response received: %@", response);
     
     NSDictionary *jsonObj = [self getJson:data];
     NSLog(@"JSON received: %@", jsonObj);
-    NSLog(@"access_token: %@", jsonObj[@"access_token"]);
 }
 
 - (void)failedWithError:(NSError *)error {
--- HttpRequester/HttpRequester/HttpRequester.h
+++ HttpRequester/HttpRequester/HttpRequester.h
@@ -25,4 +25,9 @@
 - (void)httpPostWithURL:(NSString *)url content:(NSString *)content delegate:(id<HttpRequesterDelegate>)receiver;
 - (void)httpGetWithURL:(NSString *)url headers:(NSDictionary *)headers delegate:(id<HttpRequesterDelegate>)receiver;
 
+- (void)httpPostWithURL:(NSString *)url content:(NSString *)content callback:(void (^)(NSError *error, NSData *data))callback;
+- (void) httpGetWithURL:(NSString *)url
+                headers:(NSDictionary *)headers
+               callback:(void (^)(NSError *error, NSData *data))callback;
+
 @end
