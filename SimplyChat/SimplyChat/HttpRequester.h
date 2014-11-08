80c160150a11fca241e90334a65fe34a209dee4f
Jivko Rusev <JR@Jivkos-MacBook-Air.local>
2014-11-08 13:32:34 +0200

    chatManager

--- SimplyChat/SimplyChat/HttpRequester.h
+++ SimplyChat/SimplyChat/HttpRequester.h
@@ -8,22 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@protocol HttpRequesterDelegate<NSObject>
-
-- (void)receivedData:(NSData *)data;
-- (void)failedWithError:(NSError *)error;
-
-@optional
-
-// http://stackoverflow.com/a/405896
-- (NSCachedURLResponse *)connection:(NSURLConnection *)connection willCacheResponse:(NSCachedURLResponse *)cachedResponse;
-
-@end
-
 @interface HttpRequester : NSObject
-
-- (void)httpPostWithURL:(NSString *)url content:(NSString *)content delegate:(id<HttpRequesterDelegate>)receiver;
-- (void)httpGetWithURL:(NSString *)url headers:(NSDictionary *)headers delegate:(id<HttpRequesterDelegate>)receiver;
 
 - (void)httpPostWithURL:(NSString *)url content:(NSString *)content callback:(void (^)(NSError *error, NSData *data))callback;
 - (void) httpGetWithURL:(NSString *)url
--- SimplyChat/SimplyChat/HttpRequester.m
+++ SimplyChat/SimplyChat/HttpRequester.m
@@ -10,33 +10,6 @@
 
 @implementation HttpRequester
 
-- (void)httpPostWithURL:(NSString *)url content:(NSString *)content delegate:(id<HttpRequesterDelegate>)receiver {
-    
-    NSData *postData = [content dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
-    NSString *postLength = [NSString stringWithFormat:@"%lu",(unsigned long)postData.length];
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    [request setURL:[NSURL URLWithString:url]];
-    [request setHTTPMethod:@"POST"];
-    [request setValue:postLength forHTTPHeaderField:@"Content-Length"];
-    [request setValue:@"application/x-www-form-urlencoded" forHTTPHeaderField:@"Current-Type"];
-    [request setHTTPBody:postData];
-    [request setCachePolicy:NSURLRequestReloadIgnoringCacheData]; // http://stackoverflow.com/a/405896
-    [request setTimeoutInterval:60.0];
-    
-    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-    [NSURLConnection sendAsynchronousRequest:request
-                                       queue:queue
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-                               
-                               if (error) {
-                                   [receiver failedWithError:error];
-                               } else {
-                                   [receiver receivedData:data];
-                               }
-                           }];
-    
-}
-
 - (void)httpPostWithURL:(NSString *)url content:(NSString *)content callback:(void (^)(NSError *error, NSData *data))callback {
     
     NSData *postData = [content dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
@@ -77,31 +50,6 @@
                                        queue:queue
                            completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {                               
                                callback(error, data);
-                           }];
-}
-
-- (void) httpGetWithURL:(NSString *)url headers:(NSDictionary *)headers delegate:(id<HttpRequesterDelegate>)receiver {
-    
-    NSMutableURLRequest *request = [[NSMutableURLRequest alloc] init];
-    [request setURL:[NSURL URLWithString:url]];
-    [request setHTTPMethod:@"GET"];
-    [request setCachePolicy:NSURLRequestReloadIgnoringCacheData]; // http://stackoverflow.com/a/405896
-    [request setTimeoutInterval:60.0];
-    
-    for (NSString* key in headers) {
-        NSString* value = [headers objectForKey:key];
-        [request setValue:value forHTTPHeaderField:key];
-    }
-    
-    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
-    [NSURLConnection sendAsynchronousRequest:request
-                                       queue:queue
-                           completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
-                               if (error) {
-                                   [receiver failedWithError:error];
-                               } else {
-                                   [receiver receivedData:data];
-                               }
                            }];
 }
 
